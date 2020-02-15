2804a9b52cbdbef0e9299537a345f172eac181d5
giulia1296 <61007402+giulia1296@users.noreply.github.com>
2020-02-15 15:35:00 +0100

    Update STRING_dataset2.py

--- Part2/CreateDatasets/STRING/STRING_dataset2.py
+++ Part2/CreateDatasets/STRING/STRING_dataset2.py
@@ -1,14 +1,14 @@
 
 from Bio import SeqIO
 
-# Creating list of original proteins
+# Creating the list of original proteins
 original_proteins = []
 with open("Original_dataset.txt") as original:
   for i in original:
     original_proteins.append(i[:-1])
 
 
-# Creating list of converted ids
+# Creating list of converted IDs
 fasta_sequences = SeqIO.parse(open('string_converted.fasta'),'fasta')
 string_proteins = []
 for fasta in fasta_sequences:
@@ -16,10 +16,10 @@ for fasta in fasta_sequences:
   proteins = name.split("|")[1]
   string_proteins.append(proteins)
 
-# Make the union to create STRING dataset
+# Make the union to create new STRING dataset
 string = set(original_proteins).union(string_proteins)
 
-# Save the dataset
+# Save the new STRING dataset in txt file
 with open('STRING_dataset.txt', 'w') as f:
     for item in string:
         f.write("%s\n" % item)
